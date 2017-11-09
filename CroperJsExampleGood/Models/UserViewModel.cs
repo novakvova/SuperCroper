f21b426c2b7ff7e624460214e817338aefae80e3
novakvova <novakvova@gmail.com>
2017-11-09 20:55:13 +0200

    Best

---
+++ CroperJsExampleGood/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CroperJsExampleGood.Models
+{
+    public class UserChangeImageViewModel
+    {
+        [Required(ErrorMessage = "Обов'язкове поле")]
+        [Display(Name = "Зображення")]
+        public string Image { get; set; }
+    }
+}
